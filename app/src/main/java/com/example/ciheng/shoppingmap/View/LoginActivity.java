6d95179382883e5cf6e4c8ffe531baeb48086dfa
ciheng <mengciheng@gmail.com>
2018-07-21 23:42:08 +0800

    lajigit

--- app/src/main/java/com/example/ciheng/shoppingmap/View/LoginActivity.java
+++ app/src/main/java/com/example/ciheng/shoppingmap/View/LoginActivity.java
@@ -22,7 +22,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.ciheng.shoppingmap.Data.userData;
+import com.example.ciheng.shoppingmap.Adapter.userAdapter;
 import com.example.ciheng.shoppingmap.R;
 
 import org.json.JSONArray;
@@ -53,7 +53,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private String password;
     private boolean flag;
     private final String serverURL="http://api.a17-sd207.studev.groept.be";
-    private userData mUserData;
+
+    userAdapter user= (userAdapter)getApplication();
 
     @SuppressLint("WrongViewCast")
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,20 +116,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                             for (int i=0;i< response.length();i++)
                             {
                                 JSONObject Event =response.getJSONObject(i);
-                                String UN=Event.getString("username");
+                                String UN=Event.getString("mUserName");
                                 String PS=Event.getString("password");
-                                int id_user=Event.getInt("id_user");
                                 username=UN;
                                 password=PS;
-
                                 if(email_tbc.equals(username))
                                 {
                                     if(password_tbc.equals(password))
                                     {
                                         checkfor=true;
-                                        mUserData=new userData();
-                                        mUserData.setUserName(username);
-                                        mUserData.setUserId(id_user);
+                                        user.setUserName(username);
                                         Intent intent = new Intent(LoginActivity.this,navigationActivity.class);
                                         startActivity(intent);
 
