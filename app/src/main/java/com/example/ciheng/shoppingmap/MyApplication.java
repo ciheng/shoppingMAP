c018557eb494cc346983394ee9e31b526a92f1e3
ciheng <mengciheng@gmail.com>
2018-07-21 08:22:25 +0800

    trytry

--- app/src/main/java/com/example/ciheng/shoppingmap/MyApplication.java
+++ app/src/main/java/com/example/ciheng/shoppingmap/MyApplication.java
@@ -6,30 +6,26 @@ import android.app.Application;
  */
 
 public class MyApplication extends Application {
-    public static String username; //name用public声明
+    public static String username;
 
     @Override
 
     public void onCreate() {
 
         super.onCreate();
-
         setName(NAME); //初始化全局变量
 
     }
 
-    public String getName() {//调用此函数可以获得name的值.
-
+    public String getName() {
         return username;
 
     }
 
-    public void setName(String name) {//调用此函数可以改变name的值，name是一个字符串类型的数据.
-
+    public void setName(String name) {
         this.username = name;
 
     }
 
-    private static final String NAME = "MyApplication";//将name初始化为Name.
-
+    private static final String NAME = "MyApplication";
 }
