728422970a438e98a7251b51962350235a877f32
ciheng <mengciheng@gmail.com>
2018-07-24 12:34:08 +0800

    cardview

---
+++ app/src/main/java/com/example/ciheng/shoppingmap/Adapter/productAdapter.java
@@ -0,0 +1,68 @@
+package com.example.ciheng.shoppingmap.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ciheng.shoppingmap.Data.product;
+import com.example.ciheng.shoppingmap.R;
+
+import java.util.List;
+
+
+
+
+public class productAdapter extends RecyclerView.Adapter<productAdapter.ViewHolder>{
+
+    private Context mContext;
+    private List<product> mProductList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardview;
+        ImageView Productimage;
+        TextView ProductName;
+        TextView ProductPrice;
+        TextView ProdectDescription;
+
+        public ViewHolder(View view) {
+            super(view);
+            cardview = (CardView) view;
+           Productimage = (ImageView) view.findViewById(R.id.photo);
+            ProductName = (TextView) view.findViewById(R.id.item_name);
+            ProductPrice= (TextView) view.findViewById(R.id.price);
+            ProdectDescription= (TextView) view.findViewById(R.id.description);
+
+        }
+    }
+
+    public productAdapter(List<product> mProductList) {
+        this.mProductList = mProductList;
+    }
+
+    @Override
+    public productAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mContext == null) {
+            mContext = parent.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(productAdapter.ViewHolder holder, int position) {
+        product Product = mProductList.get(position);
+        holder.ProductName.setText(Product.getName());
+       //Glide.with(mContext).load(Product.getImageId()).into(holder.Productimage);            //Glide是加载图片的方式
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProductList.size();
+    }
+
+}
--- app/src/main/java/com/example/ciheng/shoppingmap/View/productList.java
+++ app/src/main/java/com/example/ciheng/shoppingmap/View/productList.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +22,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.ciheng.shoppingmap.Adapter.productAdapter;
+import com.example.ciheng.shoppingmap.Data.product;
 import com.example.ciheng.shoppingmap.Data.userData;
 import com.example.ciheng.shoppingmap.R;
 
@@ -27,27 +32,41 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class productList extends AppCompatActivity {
-    private static final String TAG = "productList";
-    userData user= (userData)getApplication();
+    private DrawerLayout drawerLayout;
 
-    private ListView mListView;
-     ArrayList item_name= new ArrayList();
-     ArrayList photo=new ArrayList();
-     ArrayList item_intro=new ArrayList();
-     ArrayList item_price=new ArrayList();
+    private static final String TAG = "productList";
+    userData user = (userData) getApplication();
+    private int mUserId;
+
+    private productAdapter adapter;
+    private List<product> fruitList=new ArrayList<>();
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selling_list);
+        setContentView(R.layout.card_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        mUserId = intent.getIntExtra("user_id", -1);
 
-        mListView = (ListView) findViewById(R.id.item_list);
+        setContentView(R.layout.activity_product_list);
 
-        mListView.setAdapter(new MyBaseAdapter());
+        getItem();
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager=new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter=new productAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -59,133 +78,48 @@ public class productList extends AppCompatActivity {
         });
     }
 
-    RequestQueue data = Volley.newRequestQueue(this);
+    private void getItem()
 
-    String url="http://api.a17-sd207.studev.groept.be/selling_item";
-    JsonArrayRequest request=new JsonArrayRequest(Request.Method.GET, url, null,
-            new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    try {
+    {
+        RequestQueue data = Volley.newRequestQueue(this);
 
-                        for (int i=0;i< response.length();i++)
-                        {
-                            JSONObject Event =response.getJSONObject(i);
-                            String UN=Event.getString("mUserName");
-                            if(UN.equals(user.getUserName()))
-                            {
-                                int PT=Event.getInt("item_photo");
-                                String IN=Event.getString("item_name");
-                                String IT=Event.getString("introduction");
-                                String PZ=Event.getString("price");
-                                item_name.add(IN);
-                                item_intro.add(IT);
-                                item_price.add(PZ);
-                                photo.add(PT);
+        String url = "http://api.a17-sd207.studev.groept.be/selling_list" + mUserId;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject Event = response.getJSONObject(i);
+                                String UN = Event.getString("mUserName");
+                                if (UN.equals(user.getUserName())) {
+                                    int PT = Event.getInt("item_photo");
+                                    String IN = Event.getString("item_name");
+                                    String IT = Event.getString("introduction");
+                                    String PZ = Event.getString("price");
+                                    product P=null;
+                                    P.setDescreption(IT);
+                                    P.setName(IN);
+                                    P.setPrice(PZ);
+                                    fruitList.add(P);
+                                }
+
                             }
-
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
-                }
-            }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            error.printStackTrace();
-        }
-    });
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
 
+        });
+        data.add(request);
 
-
-
-
-    private class MyBaseAdapter implements ListAdapter {
-        @Override
-        public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
-        }
-
-        @Override
-        public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-        }
-
-        @Override
-
-        public int getCount() {
-            return item_name.size();
-
-        }
-
-
-
-        @Override
-
-        public Object getItem(int position) {
-            return item_name.get(position);
-
-        }
-
-
-
-        @Override
-
-        public long getItemId(int position) {
-            return position;
-
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return false;
-        }
-
-
-        @Override
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            View view=View.inflate(productList.this,R.layout.content_lv_list,null);
-
-            TextView mTextView1=(TextView) view.findViewById(R.id.item_list);
-            ImageView imageView=(ImageView)view.findViewById(R.id.item_photo);
-            TextView mTextView2=(TextView) view.findViewById(R.id.item_intro);
-            TextView mTextView3=(TextView) view.findViewById(R.id.item_price);
-
-
-            mTextView1.setText((CharSequence) item_name.get(position));
-            imageView.setBackgroundResource((Integer) photo.get(position));
-            mTextView2.setText((CharSequence) item_intro.get(position));
-            mTextView3.setText((CharSequence) item_price.get(position));
-
-            return view;
-
-        }
-
-        @Override
-        public int getItemViewType(int i) {
-            return 0;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean areAllItemsEnabled() {
-            return false;
-        }
-
-        @Override
-        public boolean isEnabled(int i) {
-            return false;
-        }
     }
+
+
 }
