5650bc81b9ab94c721bf455874e59c48ac18f2b7
ciheng <mengciheng@gmail.com>
2018-07-29 10:34:29 +0800

    跟着教程做了个不知道能不能用的messagelist

---
+++ app/src/main/java/com/example/ciheng/shoppingmap/Adapter/listAdapter.java
@@ -0,0 +1,90 @@
+package com.example.ciheng.shoppingmap.Adapter;
+
+import android.widget.BaseAdapter;
+
+import java.util.List;
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.view.LayoutInflater;
+
+import com.example.ciheng.shoppingmap.Data.message;
+import com.example.ciheng.shoppingmap.R;
+
+/**
+ * Created by 39112 on 2018/7/28.
+ */
+
+public class listAdapter extends BaseAdapter {
+    Context context;
+
+    List<message> TempSubjectList;
+
+    public listAdapter(List<message> listValue, Context context)
+    {
+        this.context = context;
+        this.TempSubjectList = listValue;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return this.TempSubjectList.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return this.TempSubjectList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ViewItem viewItem = null;
+
+        if(convertView == null)
+        {
+            viewItem = new ViewItem();
+
+            LayoutInflater layoutInfiater = (LayoutInflater)this.context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            convertView = layoutInfiater.inflate(R.layout.list_item, null);
+
+            viewItem.IdTextView = (TextView)convertView.findViewById(R.id.BuyerName);
+
+            viewItem.NameTextView = (TextView)convertView.findViewById(R.id.buyerMsg);
+
+            convertView.setTag(viewItem);
+        }
+        else
+        {
+            viewItem = (ViewItem) convertView.getTag();
+        }
+
+        viewItem.IdTextView.setText(TempSubjectList.get(position).senderID);
+
+        viewItem.NameTextView.setText(TempSubjectList.get(position).message);
+
+        return convertView;
+    }
+
+
+
+}
+
+class ViewItem
+{
+    TextView IdTextView;
+    TextView NameTextView;
+
+}
---
+++ app/src/main/java/com/example/ciheng/shoppingmap/Data/message.java
@@ -0,0 +1,11 @@
+package com.example.ciheng.shoppingmap.Data;
+
+/**
+ * Created by 39112 on 2018/7/28.
+ */
+
+public class message {
+    public String senderName;
+    public String message;
+    public String senderID;
+}
