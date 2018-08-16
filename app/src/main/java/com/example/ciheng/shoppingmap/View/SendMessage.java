0b5cf2050370e6a083586c00cfe1cc4619656700
ciheng <mengciheng@gmail.com>
2018-08-16 17:40:22 +0800

    sendMessage的layout疯了

--- app/src/main/java/com/example/ciheng/shoppingmap/View/SendMessage.java
+++ app/src/main/java/com/example/ciheng/shoppingmap/View/SendMessage.java
@@ -17,6 +17,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.example.ciheng.shoppingmap.Data.product;
 import com.example.ciheng.shoppingmap.Data.userData;
 import com.example.ciheng.shoppingmap.R;
 
@@ -28,6 +30,7 @@ public class SendMessage extends AppCompatActivity {
     private TextView Seller;
     private TextView product;
     private EditText message;
+    private int sellerID;
     private String sellerName;
     private String productName;
     private String send_message;
@@ -42,13 +45,50 @@ public class SendMessage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        productName = extras.getString("product_name",null );
+        mUserId = extras.getInt("user_id", -1);
+        sellerID=extras.getInt("seller_id",-1);
+
         setContentView(R.layout.activity_send_message);
         Seller=(TextView) findViewById(R.id.Seller);
         product=(TextView) findViewById(R.id.product);
         message=(EditText) findViewById(R.id.message);
+        getSeller();
         Seller.setText(sellerName);
         product.setText(productName);
     }
+
+    private void getSeller() {
+        RequestQueue data = Volley.newRequestQueue(this);
+        String url = "http://api.a17-sd207.studev.groept.be/findUser_byID/" + sellerID;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject Event = response.getJSONObject(i);
+                                sellerName = Event.getString("username");
+                                sellerName = sellerName.replaceAll("%20", " ");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+        });
+        data.add(request);
+
+    }
+
 
     private void send(View view)
     {
